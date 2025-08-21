893c1214757e5b312cef4ee797e296975307e60b
Andrew-Dev <145460945+Andrew-Ndong@users.noreply.github.com>
2025-08-21 01:53:08 +0100

    Ajout d'un repertoire date contenant une liste d'étudiant

---
+++ Frontend/src/data/listeEtudiant.jsx
@@ -0,0 +1,12 @@
+export const listeEtudiant = [
+    {
+        id: 1,
+        matricule: '1464-ESGIS-GA-24',
+        nom: 'NDONG NGOUA',
+        prenom: 'Andrew Keci',
+        classe: '5A',
+        filiere: 'IIRT',
+        niveau: 'Master 1',
+        statut: 'Boursier'
+    },
+]
